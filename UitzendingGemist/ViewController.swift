ea23025ac70c05e1de066526049dbba67916b7a9
Jeroen Wesbeek <github@osx.eu>
2016-07-13 14:01:21 +0200

    Initial Commit

---
+++ UitzendingGemist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UitzendingGemist
+//
+//  Created by Jeroen Wesbeek on 13/07/16.
+//  Copyright © 2016 Jeroen Wesbeek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
