0cde420599b9c7fb0ae596e0f932cc140fc05617
Jeroen Wesbeek <github@osx.eu>
2016-08-02 16:20:29 +0200

    Changed the to program button

--- UitzendingGemist/ToProgramButton.swift
+++ UitzendingGemist/ToProgramButton.swift
@@ -11,17 +11,17 @@ import UIKit
 import CocoaLumberjack
 
 class ToProgramButton: UIButton {
-    let focusedImageName = "Television-black"
-    let unfocusedImageName = "Television-white"
+//    let focusedImageName = "Television-black"
+//    let unfocusedImageName = "Television-white"
     
-    internal var televisionImageView: UIImageView?
+//    internal var televisionImageView: UIImageView?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        let televisionImageView = UIImageView(image: UIImage(named: self.unfocusedImageName))
-        self.addSubview(televisionImageView)
-        self.televisionImageView = televisionImageView
+//        let televisionImageView = UIImageView(image: UIImage(named: self.unfocusedImageName))
+//        self.addSubview(televisionImageView)
+//        self.televisionImageView = televisionImageView
     }
     
     override func canBecomeFocused() -> Bool {
@@ -31,6 +31,7 @@ class ToProgramButton: UIButton {
     override func didUpdateFocusInContext(context: UIFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
         super.didUpdateFocusInContext(context, withAnimationCoordinator: coordinator)
         
-        self.televisionImageView?.image = UIImage(named: self.focused ? self.focusedImageName : self.unfocusedImageName)
+//        self.televisionImageView?.image = UIImage(named: self.focused ? self.focusedImageName : self.unfocusedImageName)
+//        self.televisionImageView?.adjustsImageWhenAncestorFocused
     }
 }
